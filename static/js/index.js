6c7927dbd2b0ea7212ad5df1f650113e9840ea99
trox115 <tofernandes67@gmail.com>
2019-10-31 17:16:57 +0000

    Created array of winning combinations

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,16 @@
 const cells = document.querySelectorAll(".cell");
 const player1 = "X";
 const player2 = "O";
+const win = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+  [1,5,9],
+  [3,5,7],
+  [1,4,7],
+  [3,6,9],
+  [2,5,8]
+]
 
 function start() {
   for (let i = 0; i < cells.length; i++) {
