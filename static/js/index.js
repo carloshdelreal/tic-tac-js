a077c036cba5a181d6a1615681fadffd70ad209b
Carlos Del Real <carloshdelreal@gmail.com>
2019-10-31 23:17:02 -0500

    game working, pending account for ties

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 // PLAYER FACTORY
 const Player = (name) => {
   let playerName = name;
-  let playerSym;
-  const plays = [];
+  let playerSym = null;
+  let plays = [];
   const getName = () => {
-    playerName;
+    return playerName;
   };
   const addPlay = (play) => {
     plays.push(play);
@@ -18,7 +18,11 @@ const Player = (name) => {
   const getSym = () => {
     return playerSym;
   };
-  return { getName, addPlay, getPlays, setSym, getSym };
+  const resetPlays = () => {
+    plays = [];
+  };
+
+  return { getName, addPlay, getPlays, setSym, getSym, resetPlays };
 };
 
 // GAME BOARD MODULE
@@ -36,11 +40,12 @@ const Board = () => {
   ];
 
   function checkWin(player) {
-    for (let i = 0; i <= win.length; i++) {
-      if (win[i].every((j) => player.getPlays.includes(j))) {
-        alert("Winner ");
+    for (let i = 0; i < win.length; i++) {
+      if (win[i].every((j) => player.getPlays().includes(j))) {
+        return true;
       }
     }
+    return false;
   }
 
   function putSymbol(cellId, player) {
@@ -50,6 +55,7 @@ const Board = () => {
   function start() {
     for (let i = 0; i < cells.length; i++) {
       cells[i].addEventListener("click", TicTac.cellClick, false);
+      cells[i].innerText = "";
     }
   }
 
@@ -65,14 +71,18 @@ let TicTac = (function() {
   let board = Board();
 
   let publicTicTac = {};
+
   const pickTurn = () => {
-    let turnFor = prompt("Who wants to play First, Player 1 | Player 2 | Random ");
+    let turnFor = prompt(
+      `Who wants to play First, ${Player1.getName()} | ${Player2.getName()} | Random `,
+      "Random"
+    );
     turnFor = turnFor.toLowerCase();
     switch (turnFor) {
-      case "player 1":
+      case `${Player1.getName()}`.toLowerCase():
         turn = Player1;
         break;
-      case "player 2":
+      case `${Player2.getName()}`.toLowerCase():
         turn = Player2;
         break;
       case "random":
@@ -85,12 +95,71 @@ let TicTac = (function() {
     }
   };
 
+  const setPlayerSymbol = () => {
+    let selectedSym = prompt(`Select ${turn.getName()} symbol X | O | Random `, "Random");
+    selectedSym = selectedSym.toLowerCase();
+    switch (selectedSym) {
+      case "x":
+        turn.setSym("X");
+        flipTurn();
+        turn.setSym("O");
+        flipTurn();
+        break;
+      case "o":
+        turn.setSym("O");
+        flipTurn();
+        turn.setSym("X");
+        flipTurn();
+        break;
+      case "random":
+        // Random 1 or 2
+        turn.setSym(["X", "O"][[0, 1][Math.floor(Math.random() * 2)]]);
+        if (turn.getSym() == "X") {
+          flipTurn();
+          turn.setSym("O");
+          flipTurn();
+        } else {
+          turn.setSym("O");
+          flipTurn();
+          turn.setSym("X");
+          flipTurn();
+        }
+        break;
+      default:
+        alert("You put an invalid entry");
+        setPlayerSymbol();
+    }
+  };
+
   const flipTurn = () => {
     if (turn === Player1) {
       turn = Player2;
     } else {
       turn = Player1;
     }
+  };
+
+  const screenUpdate = () => {
+    document.querySelector(".turnFor").innerHTML = turn.getName();
+  };
+
+  const beginGame = () => {
+    board.start();
+    screenUpdate();
+  };
+
+  const winner = () => {
+    let end = document.querySelector(".endgame");
+    end.style.display = "block";
+    end.innerHTML = `Congratulations ${turn.getName()}, you Win!`;
+    document.querySelector(".turnFor").innerHTML = `${turn.getName()}, Won!`;
+  };
+  publicTicTac.playAgain = () => {
+    Player1.resetPlays();
+    Player2.resetPlays();
+    document.querySelector(".endgame").style.display = "none";
+
+    beginGame();
   };
 
   publicTicTac.startGame = function() {
@@ -107,26 +176,28 @@ let TicTac = (function() {
     // Set The Turn
     pickTurn();
 
+    // Set symbol
+    setPlayerSymbol();
+
     // start playing
-    board.start();
+    beginGame();
   };
 
   publicTicTac.cellClick = (cell) => {
-    // if (lastplay === "p2") {
-    //   // Turn Flip
-    //   putSymbol(cell.target.id, player1); // board update
-    //   lastplay = "p1";
-    //   player1_plays.push(parseInt(cell.target.id, 10)); // update player's plays
-    //   checkWin(player1_plays, lastplay); // check win
-    // } else {
-    //   putSymbol(cell.target.id, player2);
-    //   lastplay = "p2";
-    //   player2_plays.push(parseInt(cell.target.id, 10));
-    //   checkWin(player2_plays, lastplay);
-    // }
-    board.putSymbol(cell, turn);
+    turn.addPlay(parseInt(cell.target.id), 10);
+    board.putSymbol(cell.target.id, turn);
+
+    if (board.checkWin(turn)) {
+      winner();
+      return null;
+    }
+    flipTurn();
+    screenUpdate();
   };
+
+  return publicTicTac;
 })();
 
-start();
-TicTac.startGame();
+document.getElementById("startBtn").addEventListener("click", TicTac.startGame);
+
+document.getElementById("playAgainBtn").addEventListener("click", TicTac.playAgain);
